91993d8e9d38e15584816de1cf271928b6abdb99
Adina Kaiymova <adina.kaiymova@gmail.com>
2019-06-17 18:07:15 +0900

    .

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -1,72 +1,88 @@
 <template>
-<v-container>
+  <v-container>
     <v-layout row wrap>
-        <v-flex xs12>
-            <h2>Login to Globomantics</h2>
-        </v-flex>
-        <v-flex xs12 sm6 offset-sm3>
-            <v-text-field
-            label="E-mail"
-            v-model="email"
-            v-bind:rules="emailRules"
-            required>
-            </v-text-field>
-             <v-text-field
-            label="Password"
-            v-model="password"
-            v-bind:rules="passwordRules"
-            v-bind:type="'password'"
-            required>
-            </v-text-field>
-        </v-flex>
-        <v-flex xs12 sm6 offset-sm3>
-            <v-btn color="error" v-on:click="cancel">Cancel</v-btn>
-            <v-btn color="success" v-on:click="login">Login</v-btn>
-        </v-flex>
+      <v-flex xs12>
+        <h2>Login to Meditlink</h2>
+      </v-flex>
+      <v-spacer></v-spacer>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-text-field
+          label="E-mail"
+          v-model="email"
+          v-bind:rules="emailRules"
+          required>
+        </v-text-field>
+      </v-flex>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-text-field
+          label="Password"
+          v-model="password"
+          v-bind:rules="passwordRules"
+          v-bind:type="'password'"
+          required>
+        </v-text-field>
+      </v-flex>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-btn v-on:click="cancel">Cancel</v-btn>
+        <v-btn color="primary" v-on:click="login">Login</v-btn>
+      </v-flex>
     </v-layout>
     <v-snackbar
-      v-model="showAlert"
       :timeout="6000"
       :top="true"
+      v-model="showAlert"
     >
-    {{ message }}
+      {{ loginError }}
+      <v-btn flat color="pink" v-on:click="showAlert = false">Close</v-btn>
     </v-snackbar>
-
-</v-container>
+  </v-container>
 </template>
 
 <script>
 export default {
-    data(){
-        return{
-            showAlert: false,
-            message:'', 
-            email: '',
-            emailRules: [
-                 v => !!v || 'E-mail is required',
-                 v => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
-            ],
-            password: '',
-            passwordRules: {
-                required: value => !!value || 'Required.',
-                min: v => v.length >= 8 || 'Min 8 characters',
-                emailMatch: () => ('The email and password you entered don\'t match')
-                }
-
-        }
-    },
-    methods: {
-        login: function(){
-            const vm = this
-            if(vm.password === 'test11'){
-            this.$router.push({ path: '/' })}
-            else{
-               vm.showAlert = true
-               vm.message = 'Email or password invalid'
-            }
-        }
+  data () {
+    return {
+      showAlert: false,
+      message: '',
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
+      ],
+      password: '',
+      passwordRules: [
+        v => !!v || 'Password is required'
+      ]
     }
-
+  },
+  computed: {
+    isLoggedIn () {
+      return this.$store.getters.isLoggedIn
+    },
+    loginError () {
+      return this.$store.getters.loginError
+    }
+  },
+  methods: {
+    login: function () {
+      const vm = this
+      const payload = {
+        email: this.email,
+        password: this.password
+      }
+      this.$store.dispatch('logInUser', payload)
+        .then(() => {
+          if (vm.isLoggedIn) {
+            this.$router.push({ path: '/' })
+          } else {
+            vm.showAlert = true
+          }
+        })
+    },
+    cancel: function () {
+      console.log('The user does not want to login!')
+    }
+  }
 }
 </script>
 
--- client/src/main.js
+++ client/src/main.js
@@ -4,14 +4,20 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 import App from './App'
 import router from './router'
+import store from './store'
+import axios from 'axios'
 
 Vue.config.productionTip = false
 
 Vue.use(Vuetify)
 
+Vue.axios = Vue.prototype.$http = axios.create({
+  baseURL: 'http://localhost:8081/api'
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   router,
   components: { App },
   template: '<App/>'
